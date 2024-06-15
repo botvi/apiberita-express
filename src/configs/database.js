35c19186bac903bfcbdf474603908d6f62920373
botvi <younoob07mei@gmail.com>
2024-06-15 20:19:33 +0700

    deploy

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,10 @@
 //database.js
 const mysql = require('mysql');
 const db = mysql.createPool({
-  host: 'bnlq5mgcgfkocuinoy8h-mysql.services.clever-cloud.com',
-  user: 'uel1kswlffjpwlrk',
-  password: 'TfeZeNf7SBPgw0jCw9XD',
-  database: 'bnlq5mgcgfkocuinoy8h'
+  host: 'asuna.kawaiihost.net',
+  user: 'jznzyogy_yoviard',
+  password: '@yovi2003',
+  database: 'jznzyogy_apiberitaexpress'
 });
 
 module.exports = db;
